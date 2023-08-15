cee8fb022d5bb950225bfd24c843640cbc882454
bindi <pankhudi@gmail.com>
2023-08-15 09:58:49 -0700

    adding examples

--- README.md
+++ README.md
@@ -1,9 +1,7 @@
 # How to use FlumeAI to log your openAI calls
 
-## Vanilla OpenAI (python)
-If you are using openAI APIs directly, following the following steps
-
 #### (1) Set the openAI base url to flumeAI
+<normal> You can see this in app.py. </normal>
 
 ```python
 openai.api_base = "https://flume-proxy.pankhudi.workers.dev/v1
@@ -11,7 +9,7 @@ openai.api_base = "https://flume-proxy.pankhudi.workers.dev/v1
 
 #### (2) Pass Flume API key as a header param to openAI API. 
 
-<normal>You should find the api key in the apps tab of your FlumeAI dashboard.</normal>
+<normal>You should find the api key in the apps tab of your FlumeAI dashboard. Pass this API key in the headers. You can find relevant examples in app.py</normal>
 
 ```python
 headers={
@@ -19,10 +17,22 @@ headers={
 }
 ```
 
-#### (3) Optional - How to pass tags 
+#### (3) Optional - How to log tags
 
-<normal>You can choose to identify you openAI with some tags. These tags will show alongside each API request on the requests tab of your FlumeAI dashbaord. You will be able to filter via these tags.</normal>
+<normal>You can choose to identify you openAI with some tags. These tags will show alongside each API request on the requests tab of your FlumeAI dashbaord. You will be able to filter via these tags. You can pass these tags in the headers. See examples in app.py.</normal>
 
-#### (4) Optional - How to pass properties 
+#### (4) Optional - How to log properties
 
-<normal>You can set pass specific identifiers such as sessionId, conversationId, etc, by passing them to FlumeAI. These properties will show alongside each API request on the requests tab of your FlumeAI dashboard. This will allow to you filter by specific property values.</normal>
+<normal>You can set pass specific identifiers such as sessionId, conversationId, etc, by passing them to FlumeAI. These properties will show alongside each API request on the requests tab of your FlumeAI dashboard. This will allow to you filter by specific property values. You can pass properties via headers. See examples in app.py.</normal>
+
+#### (5) How to run examples in this repo.
+
+<normal>First clone this repo and add your openAI API key in .env file. In the main directory, start up Python VM, install requirements and start Flask server as shown below. Once the server is running at 5000, use the respective endpoints in app.py to test different use cases. 
+
+```python
+python3 -m venv venv
+. venv/bin/activate
+pip3 install -r requirements.txt
+flask run
+flask run --port xxxx  //to run on a specific port 
+```
---
+++ app.py
@@ -0,0 +1,171 @@
+import os
+from dotenv import load_dotenv
+import openai
+
+from flask import Flask, jsonify, request
+from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+from langchain.chains import SimpleSequentialChain
+from langchain.agents import load_tools
+from langchain.agents import initialize_agent
+from langchain.agents import AgentType
+from langchain.prompts import (
+    PromptTemplate,
+)
+from langchain.memory import ConversationBufferWindowMemory
+from langchain.chat_models import ChatOpenAI
+from langchain import PromptTemplate, LLMChain
+
+from langchain.schema import (
+    HumanMessage,
+    SystemMessage
+)
+
+load_dotenv()
+app = Flask(__name__)
+openai.api_key = os.getenv("OPENAI_API_KEY")
+print(os.getenv("FLASK_APP"))
+
+# add following line to start logging your openAI requests using Flume
+openai.api_base = "https://oai.flumeai.workers.dev/v1"
+
+@app.route("/", methods=["GET"])
+def index():
+    return jsonify("Not implemented. Check app.py for valid routes.")
+
+# vanilla open AI single shot example
+@app.route("/flume/simple", methods=["GET"])
+def get_completion_simple():
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt="I want a short childen's story about a boy who goes to the moon.",
+        temperature=0.2,
+        headers={
+            "Flume-API-Key": "ADD_YOUR_FLUME_API_KEY_HERE",
+            "Flume-Metadata-Session": "session-simple",
+            "Flume-Metadata-Conversation": "conversation-simple",
+            "Flume-Tags": "promptchain1-simple,openAI-vanilla",
+        },
+    )
+    return jsonify(response)
+
+# simple one short example using langchain 
+@app.route("/flume/langchain/simple", methods=["GET"])
+def get_completion_langchain_simple():
+    llm = OpenAI(temperature=0.6,
+                 n=1,
+                 headers={
+                     "Flume-API-Key": "ADD_YOUR_FLUME_API_KEY_HERE",
+                     "Flume-Metadata-Session": "session-langchain-simple",
+                     "Flume-Metadata-Conversation": "conversation-langchain-simple",
+                     "Flume-Tags": "promptchain-langchain",
+                 })
+
+    prompt = PromptTemplate(
+        input_variables=["pet"],
+        template="Give me one name for a small {pet}?",
+    )
+
+    chain = LLMChain(llm=llm, prompt=prompt)
+    response = chain.run("dog")
+    return jsonify(response)
+
+# TO-FIX - header is not getting passed to flume
+@app.route("/flume/langchain/chat2", methods=["GET"])
+def get_completion_langchain_chat2():
+    chat = ChatOpenAI(temperature=0.6,
+                 headers={
+                     "Flume-API-Key": "ADD_YOUR_FLUME_API_KEY_HERE",
+                     "Flume-Metadata-Session": "session-1234-chat2",
+                     "Flume-Metadata-Conversation": "issue2345-chat2",
+                     "Flume-Tags": "promptchain1-chat2,production-chat2",
+                 })
+
+    batch_messages = [
+    [
+        SystemMessage(content="You are a helpful assistant that translates English to Spanish."),
+        HumanMessage(content="I love programming.")
+    ],
+    [
+        SystemMessage(content="You are a helpful assistant that translates English to Flemish."),
+        HumanMessage(content="I love artificial intelligence.")
+    ],
+]
+    result = chat.generate(batch_messages)
+    print(result)
+    return jsonify({"result": "check console"})
+
+# langchain example with sequential chains
+@app.route("/flume/langchain/seq", methods=["GET"])
+def get_completion_langchain_seq():
+    llm = OpenAI(temperature=0.9,
+                 headers={
+                     "Flume-API-Key": "ADD_YOUR_FLUME_API_KEY_HERE",
+                     "Flume-Metadata-Session": "session-langchain-seq",
+                     "Flume-Metadata-Conversation": "conversation-langchain-seq",
+                     "Flume-Tags": "promptchain-seq,production-seq",
+                 })
+
+    prompt1 = PromptTemplate(
+        input_variables=["animal"],
+        template="Give me one name for {animal}?",
+    )
+
+    prompt2 = PromptTemplate(
+        input_variables=["name"],
+        template="Write a sonnet for this {name}?",
+    )
+
+    chain1 = LLMChain(llm=llm, prompt=prompt1)
+    chain2 = LLMChain(llm=llm, prompt=prompt2)
+    overall_chain = SimpleSequentialChain(
+        chains=[chain1, chain2], verbose=True)
+    response = overall_chain.run("dog")
+    return jsonify(response)
+
+
+# langchain example for chat
+@app.route("/flume/langchain/chat", methods=["GET"])
+def get_completion_langchain_chat():
+    llm = OpenAI(temperature=0.9, headers={
+                     "Flume-API-Key": "ADD_YOUR_FLUME_API_KEY_HERE",
+                     "Flume-Metadata-Session": "session-langchain-seq-chat",
+                     "Flume-Metadata-Conversation": "conversation-langchain-seq-chat",
+                     "Flume-Tags": "promptchain-seq-chat,production-seq-chat",
+    })
+
+    template = "You are a helpful assistant that translates English to French.\n' \
+                {history}\n' \
+                Human: {human_input}\n' \
+                Assistant:"
+
+    prompt = PromptTemplate(
+        input_variables=["history", "human_input"],
+        template=template,
+    )
+    chatgpt_chain = LLMChain(
+        llm=llm,
+        prompt=prompt,
+        verbose=True,
+        memory=ConversationBufferWindowMemory(k=2),
+    )
+    o = chatgpt_chain.predict(human_input="Hello, how are you?")
+    print(o)
+    return jsonify(o)
+
+
+# langchain example for agent 
+@app.route("/flume/langchain/agent", methods=["GET"])
+def get_completion_langchain_agent():
+    llm = OpenAI(temperature=0, headers={
+        "Flume-API-Key": "ADD_YOUR_FLUME_API_KEY_HERE",
+        "Flume-Metadata-Session": "session-langchain-agent",
+        "Flume-Tags": "promptchain-agent,production-agent",
+    })
+    tools = load_tools(["serpapi", "llm-math"], llm=llm)
+    agent = initialize_agent(
+        tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True)
+    output = agent.run(
+        "Who is Leo DiCaprio's girlfriend? What is her current age raised to the 0.43 power?")
+    return jsonify(output)
